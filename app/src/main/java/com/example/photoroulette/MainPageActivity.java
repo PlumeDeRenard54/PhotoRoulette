ecfafa978d0d93102a5dc123c14973689f21c407
PlumeDeRenard54 <tom.aubert54@outlook.fr>
2026-01-11 14:33:40 +0100

    Bip boup bip front en java

---
+++ app/src/main/java/com/example/photoroulette/MainPageActivity.java
@@ -0,0 +1,21 @@
+package com.example.photoroulette;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.photoroulette.R;
+
+public class MainPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_page);
+
+    }
+}

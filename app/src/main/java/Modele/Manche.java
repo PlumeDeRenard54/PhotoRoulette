4986bc1b98e0d10a515acea600fa3bc7ddba955f
floflow <florian.hannezo5@etu.univ-lorraine.fr>
2026-01-11 12:45:28 +0100

    [modify] ajustement de manches et création des manches sans conenue

--- app/src/main/java/Modele/Manche.java
+++ app/src/main/java/Modele/Manche.java
@@ -1,9 +1,23 @@
 package Modele;
 
+import android.provider.MediaStore;
+
 public class Manche {
     Contenu contenu;
+    User proprietaire;
     int pointMax;
     long tickDepart;
+
+    Manche(User propietaire, int pointMax){
+            this.proprietaire = propietaire;
+            this.pointMax = pointMax;
+        }
+    void setContenu(MediaStore.Images m){
+        contenu = new ContenuImage(proprietaire,m);
+    }
+    void setContenu(MediaStore.Video m){
+        contenu = new ContenuVideo(proprietaire,m);
+    }
 
     void calculerPoint(long tickFinale, User u){
         //à revoir pour le calcul des points
--- app/src/main/java/Modele/Partie.java
+++ app/src/main/java/Modele/Partie.java
@@ -1,11 +1,13 @@
 package Modele;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Partie {
     ArrayList<User> joueurs;
     ArrayList<Manche> listeManche;
+
+    int nbManches = 10 ;
 
     void ajouterJoueur(User j){
         joueurs.add(j);
@@ -15,7 +17,13 @@ public class Partie {
         joueurs.remove(j);
     }
 
-    void creerManches(){}
+    void creerManches(){
+        listeManche = new ArrayList<Manche>();
+        for (int i =0; i < nbManches; i++){
+            User joueur = joueurs.get(ThreadLocalRandom.current().nextInt(0, joueurs.size()));
+            listeManche.add(new Manche(joueur,100));
+        }
+    }
 
     void start() {
         for(Manche m : listeManche){
